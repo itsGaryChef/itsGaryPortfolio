b6a5d4159d655fe7345e76941f177826206b8b37
itsGaryChef <gkauljr@gmail.com>
2026-09-22 22:18:43 -0500

    Make Beat Kitchen mobile friendly

--- app/beat-kitchen/page.tsx
+++ app/beat-kitchen/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import "../play/play.css";
 
 export default function BeatKitchen() {
-  return <main className="play-shell">
+  return <main className="play-shell beat-kitchen-shell">
     <Link className="play-exit" href="/arcade" aria-label="Exit Beat Kitchen and return to The Arcade">Exit to Arcade</Link>
     <iframe
       className="play-frame"
